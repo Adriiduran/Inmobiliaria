5334b03264ecd6a53b4c760442ba389133cc4629
chacuya11 <monraedo@live.com>
2023-02-23 11:04:44 +0100

    login

--- inmobiliaria/resources/views/auth/login.blade.php
+++ inmobiliaria/resources/views/auth/login.blade.php
@@ -3,12 +3,18 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
+
         @csrf
 
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <!-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" /> -->
+            <input type="email" class="form-control form-control-user"
+                                                id="email" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address..."
+                                                name="email"
+                                                :value="old('email')">
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
