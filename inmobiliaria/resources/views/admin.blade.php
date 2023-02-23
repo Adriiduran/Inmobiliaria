a7020ffa25e316d1b8bf4cae4daf2a2c55746df9
chacuya11 <monraedo@live.com>
2023-02-23 10:05:08 +0100

    ult

--- inmobiliaria/resources/views/admin.blade.php
+++ inmobiliaria/resources/views/admin.blade.php
@@ -26,17 +26,21 @@
 
 <body class="sb-nav-fixed ">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        
         <!-- Navbar Brand-->
-       <a class="navbar-brand ps-3" href="/"><i class=" fas fa-cat"></i> INMOBILIARIA</a>
+        <a class="navbar-brand ps-3" href="/"><i class=" fas fa-cat"></i> INMOBILIARIA</a>
         <!-- Sidebar Toggle-->
         <div><button class="btn btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="	fas fa-outdent"></i></button></div>
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         </form>
         
+
         <!-- Navbar-->
+
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user-cog"></i></a>
+
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
@@ -62,7 +66,8 @@
             </li>
         </ul>
     </nav>
-    <div id="layoutSidenav" >
+    
+    <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
@@ -204,7 +209,7 @@
                                     </tr>
                                 </thead>
                                 <tfoot>
-                                <tr class="table-secondary">
+                                    <tr class="table-secondary">
                                         <th>ID</th>
                                         <th>Descripción</th>
                                         <th>Dirección</th>
