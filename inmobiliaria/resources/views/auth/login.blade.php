b91c7412333ee4b6cc7e81218a4c68d80b462f65
Adriiduran <adriiiduraan@gmail.com>
2023-03-07 13:12:20 +0100

    login terminado

--- inmobiliaria/resources/views/auth/login.blade.php
+++ inmobiliaria/resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Bienvenido!!!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Inicio de Sesión</h1>
                                     </div>
                                     <!-- Session Status -->
                                     <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -35,41 +35,41 @@
                                         <div class="form-group">
                                             <x-input-label for="email" :value="__('Email')" />
                                             <!-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" /> -->
-                                            <input type="email" class="form-control form-control-user" id="email" aria-describedby="emailHelp" placeholder="Introduce Email" name="email" :value="old('email')">
+                                            <input type="email" class="form-control form-control-user" id="email" aria-describedby="emailHelp" placeholder="Introduce tu email" name="email" :value="old('email')">
                                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                         </div>
 
                                         <!-- Password -->
                                         <div class="form-group">
-                                            <x-input-label for="password" :value="__('Password')" />
+                                            <x-input-label for="password" :value="__('Contraseña')" />
 
                                             <!-- <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" /> -->
 
-                                            <input type="password" class="form-control form-control-user" id="password" aria-describedby="emailHelp" placeholder="Contraseña..." name="password" required autocomplete="current-password">
+                                            <input type="password" class="form-control form-control-user" id="password" aria-describedby="emailHelp" placeholder="Introduce tu contraseña" name="password" required autocomplete="current-password">
 
                                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
                                         </div>
 
-                                        <!-- Remember Me -->
+                                        <!-- Recordarme -->
                                         <div class="form-group">
                                             <label for="remember_me" class="inline-flex items-center">
                                                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                                                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                                                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recordarme') }}</span>
                                             </label>
                                         </div>
 
-                                        <div class="flex items-center justify-end mt-4">
+                                        <div class="d-flex justify-items-between mt-4 g-3 container-fluid">
                                             @if (Route::has('password.request'))
                                             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                                                {{ __('Forgot your password?') }}
+                                                {{ __('Has olvidad tu contraseña?') }}
                                             </a>
                                             @endif
 
                                             <button class="btn btn-primary btn-user btn-block">
-                                                {{ __('Log in') }}
+                                                {{ __('Iniciar Sesión') }}
                                             </button>
 
                                         </div>
